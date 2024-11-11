4059e60d87081bf77c62c96fd9e60ecc2c8729e7
Kurt <kurtabinion@gmail.com>
2024-11-11 17:53:45 +0800

    Delete record

--- app/product/get_products.php
+++ app/product/get_products.php
@@ -15,7 +15,7 @@
         
 
     } catch (PDOException $e) {
-        echo "Connection Failed: " . $e->getMEssage();
+        echo "Connection Failed: " . $e->getMessage();
         $db = null;
 
     }
